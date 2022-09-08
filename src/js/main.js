7df96aa7fe439c3d1e9d96c7920dfa901a8bd07f
lnkz <lnkz80@gmail.com>
2022-09-08 20:47:52 +0300

    modalWindow - inPrgrs: event added, next form processing

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import modal from "./modules/modal";
+
+document.addEventListener("DOMContentLoaded", () => {
+  modal(".popup_engineer_btn", ".popup_engineer");
+  modal(".phone_link", ".popup");
+});
---
+++ src/js/modules/modal.js
@@ -0,0 +1,35 @@
+//MODAL's open&close functions
+const openModal = (modObj) => {
+  modObj.classList.remove("hide");
+  modObj.classList.add("show");
+};
+
+const closeModal = (modObj) => {
+  modObj.classList.remove("show");
+  modObj.classList.add("hide");
+};
+
+const modal = (modTriggerSelector, modalWindowSelector) => {
+  const modTrigger = document.querySelectorAll(modTriggerSelector),
+    modalWindow = document.querySelector(modalWindowSelector);
+
+  //open modal window event
+  modTrigger.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      openModal(modalWindow);
+    });
+  });
+
+  //close modal window event (add "data-close attr to index.html")
+  modalWindow.addEventListener("click", (e) => {
+    if (
+      e.target === modalWindow ||
+      e.target.parentElement.getAttribute("data-close") == ""
+    ) {
+      closeModal(modalWindow);
+    }
+  });
+};
+
+export default modal;
