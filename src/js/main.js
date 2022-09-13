2de2ee6ebf0b1ff1d4e9a95d28174115bd9f9ef1
lnkz <lnkz80@gmail.com>
2022-09-13 23:58:08 +0300

    modalWindow - inPrgrs: postData creating

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,12 @@ import formCallMe from "./modules/form";
 import modal from "./modules/modal";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // postData();
+  const reqUrl = "https://jsonplaceholder.typicode.com/todos/1";
+  const req = postData("GET", reqUrl);
+  console.log(req);
+  // .then((data) => console.log(data))
+  // .catch((err) => console.log(err));
+
   modal(".popup_engineer_btn", ".popup_engineer");
   modal(".phone_link", ".popup");
   formCallMe(".main_form");
--- src/js/services/postData.js
+++ src/js/services/postData.js
@@ -1,5 +1,36 @@
-const postData = () => {
+//XHR
+// const postData = (reqMethod, reqUrl) => {
+//   const request = new XMLHttpRequest();
+//   request.open(reqMethod, reqUrl);
+//   request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+//   request.responseType = "JSON";
+//   request.send();
 
+//   request.addEventListener("load", () => {
+//     console.log(JSON.parse(request.response));
+//   });
+// };
+
+const postData = (reqMethod, reqUrl) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(reqMethod, reqUrl);
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response);
+      }
+    };
+
+    xhr.onerror = () => {
+      reject(xhr.response);
+    };
+
+    xhr.send();
+  });
 };
 
 export default postData;
