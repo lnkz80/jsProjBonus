b467068a2130d2495f40df39be37bb31bd454389
lnkz <lnkz80@gmail.com>
2022-09-26 23:53:16 +0300

    modalWindow - inPrgrs: postData done, spinner added, message ok added. Make Phone checking

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -4,34 +4,51 @@ const formCallMe = (formSelector) => {
   const forms = document.querySelectorAll(formSelector);
   const numbers = /^[0-9]+$/;
   forms.forEach((form) => {
-    // form.querySelector('[name="user_phone"]').addEventListener("keyup", () => {
-    //     console.log(form.querySelector('[name="user_phone"]').value);
-    // });
-    // console.log(form);
-    form.querySelector("button").addEventListener("click", (e) => {
+    form.querySelector("button").addEventListener("click", function (e) {
       e.preventDefault();
       if (!form.userPhone.value.match(numbers)) {
         alert("В полі 'Телефон' треба вводити тільки цифри!!!");
         form.userPhone.value = "";
-        // form.querySelector('[name="user_phone"]').value = "";
         return false;
       }
       //get data from form fields and convert to json
       const formData = new FormData(form);
-      // const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      //HERE MUST BE A POSTDATA FUNCTION WITH AJAX
-      const postObj = Object.fromEntries(formData.entries());
+      const postObj = Object.fromEntries(formData.entries()),
+        buttonValue = this.textContent,
+        spinner = this.querySelector("span");
 
+      //SPINNER
+      // this.innerHTML = '<img src="./assets/img/main/spinner.gif">';
+      // this.innerHTML = '<span class="submit-spinner submit-spinner_hide"></span>';
+      spinner.classList.remove('submit-spinner_hide');
+      this.disabled = true;
+      this.style.color = "grey";
+      this.style.filter = "grayscale(70%)";
       const reqUrl = "http://localhost:3000/members";
-      let nextPostId = 1;
-      postData(reqUrl)
+      // let nextPostId = 1;
+
+      postData(reqUrl, postObj)
         .then((data) => {
-          postObj.id = data.length + 1;
-          postData(reqUrl, postObj)
-            .then((data) => console.log(data))
-            .catch((err) => console.error(err));
+          console.log(data);
+          // this.textContent = buttonValue;
         })
+        .then(
+          () => {
+            spinner.classList.add('submit-spinner_hide');
+            this.disabled = false;
+            this.style.color = "black";
+            this.style.filter = "grayscale(0%)";
+            form.reset();
+            const span = document.createElement("span");
+            span.style.color = "green";
+            span.textContent = "Информация отправлена!";
+            this.parentElement.append(span);
+            setTimeout(() => {
+              span.remove();
+            }, 3000);
+          }
+        )
         .catch((err) => console.error(err));
       console.log("Data has been posted!");
     });
